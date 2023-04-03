8c118b122bd6b3471655054aca7e94e33147cabb
Karina Assini <karina.andreatta@arcelormittal.com.br>
2023-04-03 14:26:19 -0300

    feat: implementing .py

---
+++ functions.py
@@ -0,0 +1,17 @@
+def continuous_to_binary(y_array: np.array, threshold: float = None) -> list:
+    """Convert an np.array with continuous values to boolean according to the threshold specified. The threshold is defined according to the **define_best_threshold** method.
+
+    Args:
+        y_array (np.array): np.array with continuous values
+
+        threshold (str): default threshold
+
+    Returns:
+        list: list of boolean values
+    """
+
+    if threshold is None:
+        threshold = 0.5
+    y_array = [1 if i > threshold else 0 for i in y_array]
+
+    return y_array
